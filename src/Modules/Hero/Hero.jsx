d72fc7af3a65e3e901a2e31f260739a938a632e5
Abduraxmon <abduhmonxalilov17@gmail.com>
2024-04-16 00:45:36 +0500

    fixed

--- src/Modules/Hero/Hero.jsx
+++ src/Modules/Hero/Hero.jsx
@@ -9,8 +9,8 @@ const Hero = () => {
                         <h1><span className='hero-title-span'>DRIVE</span> YOUR BUSINESS</h1>
                         <p className='hero-subtitle'>With our <span className='hero-title-span'>ERP system</span> that helps You to manage warehouse and sales, and track the process with lots of features for free</p>
                         <div className='hero-end'>
-                            <a href='https://avto-sell.vercel.app/' className='hero-create-btn'>CREATE A COMPANY</a>
-                            <button className='hero-join-btn'>JOIN US NOW</button>
+                            <a href='https://661d823ab9d785216a0b70b0--zingy-llama-e807c1.netlify.app/' className='hero-create-btn'>CREATE A COMPANY</a>
+                            <button  className='hero-join-btn' ><a href="https://661d73174c952320e2ca19c8--eloquent-travesseiro-3b9bd6.netlify.app/">JOIN US NOW</a></button>
                         </div>
                     </div>
                 </div>
